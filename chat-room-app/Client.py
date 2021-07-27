3c239e46612ad6d17ee27a283fd6383769ae572f
Young wook Jeon <ela87@naver.com>
2021-07-27 22:54:33 +0900

    Initial commit

---
+++ chat-room-app/Client.py
@@ -0,0 +1,43 @@
+import tkinter
+import socket
+from tkinter import *
+from threading import Thread
+
+window = Tk()
+window.title("채팅방 애플리케이션")
+window.configure(bg="green")
+
+message_frame = Frame(window, height=100, width=100, bg="red")
+message_frame.pack()
+
+my_msg = StringVar()
+my_msg.set("")
+
+scroll_bar = Scrollbar(message_frame)
+msg_list = Listbox(message_frame, height=15, width=100, bg="red", yscrollcommand=scroll_bar.set)
+scroll_bar.pack(side=RIGHT, fill=Y)
+msg_list.pack(side=LEFT, fill=BOTH)
+msg_list.pack()
+
+label = Label(window, text="메시지를 입력하세요", fg="blue", font="Aeria", bg="red")
+label.pack()
+
+entry_field = Entry(window, textvariable=my_msg, fg="red", width=50)
+entry_field.pack()
+
+send_button = Button(window, text="Send", font="Aerial", fg="white", command=send)
+send_button.pack()
+
+quit_button = Button(window, text="종료", font="Aerial", fg="white", command=on_closing)
+quit_button.pack()
+
+Host = "127.0.0.1"
+Port = 8080
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((Host, Port))
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+
+mainloop()
---
+++ chat-room-app/Server.py
@@ -0,0 +1,57 @@
+import socket
+from threading import Thread
+
+host = 'localhost'
+port = 8080
+clients = {}
+addresses = {}
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, port))
+
+def handle_clients(conn, address):
+    name = conn.recv(1024).decode()
+    welcome = "Welcome " + name + ". You can type #quit if you want to leave the chat room."
+    conn.recv(bytes(welcome, "utf8"))
+    msg = name + " has recently joined the chat room."
+    broadcast(bytes(msg, "utf8"))
+    clients[conn] = name
+
+    while True:
+        msg = conn.recv(1024)
+        if msg != bytes("#quit", "utf8"):
+            broadcast(msg, name + ":")
+        else:
+            conn.send(bytes("#quit", "utf8"))
+            conn.close()
+            del clients[conn]
+            broadcast(bytes(name + " has left the chat room."))
+
+def accept_client_connections():
+    while True:
+        client_conn, client_address = sock.accept()
+        print(client_address, " has connected")
+        client_conn.send("Welcome to the Chat room, please type your name to continue".encode("utf8"))
+        addresses[client_conn] = client_address
+
+        Thread(target=handle_clients, args=(client_conn, client_address)).start()
+
+def broadcast(msg, prefix=""):
+    for x in clients:
+        x.send(bytes(prefix, "utf8") + msg)
+
+# sock.listen(1) # one request at one time
+# print("The server is running and is listening to client requests.")
+# conn, address = sock.accept()
+
+# message = 'Hey there is something important for you!'
+# conn.send(message.encode())
+# conn.close()
+
+if __name__ == "__main__":
+    sock.listen(5)
+    print("The server is running and is listening to client requests.")
+
+    t1 = Thread(target=accept_client_connections)
+    t1.start()
+    t1.join()
